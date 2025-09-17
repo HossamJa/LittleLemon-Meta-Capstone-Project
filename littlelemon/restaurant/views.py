1e6ccf053c8fec7183d43e2e7207cf6f79012045
HossamJa <jamhosam123@gmail.com>
2025-09-17 14:04:05 +0100

    Built Menu and Booking APIs, and created the Serializer and configured the routes

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,24 @@
+from rest_framework import generics, viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import MenuItem, Booking
+from .serializers import BookingSerializer, MenuItemSerializer
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'index.html', {})
+
+def home(request):
+    return render(request, 'index.html')
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class BookingView(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
